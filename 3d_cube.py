171ae1c2cd00b2eb69e13752c249b5da6befa2f6
Nathan Smith <nathan.smith.compsci@gmail.com>
2022-07-21 16:52:56 -0400

    Changed README

--- 3d_cube.py
+++ 3d_cube.py
@@ -8,11 +8,17 @@ screen = pg.display.set_mode((screen_width, screen_height))
 cube_center_x = screen_height//2
 cube_center_y = screen_height//2
 clock = pg.time.Clock()
+ticks = pg.time.get_ticks()
 
-initial_size = 150
-scale = 1 # How much the shape scales with each frame.
+
+# Modifiable parameters
+initial_size = 150 # Each leg of the cube will be double this value
+scale = 1 # How much the shape scales with each frame. 1 = Unchanged
 shear = False # Must be true for shearing to function
-x,y,z = 1,1,0.7 # The rate of change in the angle with respect to each dimension. (In degrees)
+drag = True # Whether drag is enabled or not. Functionally the same as drag_rate = 0 when disabled
+drag_rate = 0.00 # 1 = Max friction, 0 = No friction at all
+movement_multiplier = 0.01 # How fast we move the cube relative to cursor movement
+x,y,z = 0,0,0 # The rate of change in the angle with respect to each dimension. (In degrees)
 # Shear rates for every axis combination
 sxy = 0.02
 sxz = 0.001
@@ -30,17 +36,44 @@ points = [[-initial_size,-initial_size,initial_size], # Left top front
           [-initial_size,-initial_size,-initial_size], # Left top back
           [-initial_size,initial_size,-initial_size], # Left bottom back
           [initial_size,initial_size,-initial_size], # Right bottom back
-          [initial_size,-initial_size,-initial_size]] # Right top back
-          
+          [initial_size,-initial_size,-initial_size]] # Right top back   
+
+def center_point(point, center_x=cube_center_x, center_y=cube_center_y):
+    return (point[0]+center_x,point[1]+center_y)
+
 while True:
     clock.tick(60) # 60fps target framerate
     screen.fill((0,0,0)) # Black background
-    
+
+    for e in pg.event.get():
+        if pg.mouse.get_pressed()[0]:
+                prev_m_x,prev_m_y = m_x,m_y
+                m_x,m_y = pg.mouse.get_pos()
+                
+                y += (m_x-prev_m_x)*movement_multiplier
+                x += (m_y-prev_m_y)*movement_multiplier
+        else:
+            m_x,m_y = pg.mouse.get_pos()
+
+    if drag: # Drag calculations
+        x *= (1-drag_rate)
+        y *= (1-drag_rate)
+        z *= (1-drag_rate)
+
     for i in range(4): # Drawing lines connecting the vertices (Assuming it's a cube)
         pg.draw.line(screen, (255,255,255), (int(points[i][0])+cube_center_x, int(points[i][1])+cube_center_y) , (int(points[(i+1)%4][0])+cube_center_x, int(points[(i+1)%4][1])+cube_center_y) )
         pg.draw.line(screen, (255,255,255), (int(points[i+4][0])+cube_center_x, int(points[i+4][1])+cube_center_y) , (int(points[((i+1)%4)+4][0])+cube_center_x, int(points[((i+1)%4)+4][1])+cube_center_y) )
         pg.draw.line(screen, (255,255,255), (int(points[i][0])+cube_center_x, int(points[i][1])+cube_center_y) , (int(points[i+4][0])+cube_center_x, int(points[i+4][1])+cube_center_y) )
 
+    # Obtaining normal vector from the front face of the cube to determine direction. 
+    vec_a = np.array(points[0])-np.array(points[2])
+    vec_b = np.array(points[0])-np.array(points[1])
+
+    normal = ( vec_a[1]*vec_b[2]-vec_a[2]*vec_b[1] , vec_a[2]*vec_b[0]-vec_a[0]*vec_b[2] , vec_a[0]*vec_b[1]-vec_a[1]*vec_b[0] )
+
+    face = ( (points[1][0]+points[3][0])/2 , (points[1][1]+points[3][1])/2 ) # Center of cube face 
+    pg.draw.line(screen, (255,0,0),  center_point(face), center_point(np.array(normal)*0.0025), width=3)
+    
     # Building the transformation matrix
     transformation_matrix = np.array( ((1,0,0),(0,1,0),(0,0,1)) )
     if x:
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # 3D Cube Renderer
-This was a test to see how simple it is to manipulated a 3D object using transformation/rotation matrices.
+<<<<<<< HEAD
+A simple renderer for a 3D wire-frame cube using rotation matrices.
+=======
+This was a test to see how simple it is to manipulate 3D objects (in this case a cube) using transformation/rotation matrices.
+>>>>>>> d96ef74cb972b57fae654274104413097cf616a6
 
 ## Example
-![Gif of cube rotating](output.gif)
+
+![Gif of cube rotating](docs/output.gif)
+
+Movement with mouse (mouse isn't visible). Normal vector on front face is visible. This is with zero drag.
+
+![Gif of cube rotating](docs/output1.gif)
