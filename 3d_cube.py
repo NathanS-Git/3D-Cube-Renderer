ec005ed9c0e90dc6758d90b2f3d2d2153d877416
Nathan Smith <nathan.smith.compsci@gmail.com>
2022-08-13 11:16:17 -0400

    Added more rendering options

--- 3d_cube.py
+++ 3d_cube.py
@@ -1,110 +1,141 @@
 import numpy as np
 import pygame as pg
 
-pg.init()
-screen_width = 600
-screen_height = 600
-screen = pg.display.set_mode((screen_width, screen_height))
-cube_center_x = screen_height//2
-cube_center_y = screen_height//2
-clock = pg.time.Clock()
-ticks = pg.time.get_ticks()
-
 
 # Modifiable parameters
-initial_size = 150 # Each leg of the cube will be double this value
-scale = 1 # How much the shape scales with each frame. 1 = Unchanged
+initial_size = 150.0 # Each leg of the cube will be double this value
+scale = 0 # How much the shape scales with each frame. 1 = Unchanged
 shear = False # Must be true for shearing to function
 drag = True # Whether drag is enabled or not. Functionally the same as drag_rate = 0 when disabled
-drag_rate = 0.00 # 1 = Max friction, 0 = No friction at all
+drag_rate = 0.01 # 1 = Max friction, 0 = No friction at all
 movement_multiplier = 0.01 # How fast we move the cube relative to cursor movement
 x,y,z = 0,0,0 # The rate of change in the angle with respect to each dimension. (In degrees)
 # Shear rates for every axis combination
-sxy = 0.02
-sxz = 0.001
+sxy = 0
+sxz = 0
 syx = 0
 syz = 0
 szx = 0
 szy = 0
 
 # Eight points defining each vertex of the cube
-#               X             Y             Z
-points = [[-initial_size,-initial_size,initial_size], # Left top front
-          [-initial_size,initial_size,initial_size], # Left bottom front
-          [initial_size,initial_size,initial_size], # Right bottom front
-          [initial_size,-initial_size,initial_size], # Right top front 
-          [-initial_size,-initial_size,-initial_size], # Left top back
-          [-initial_size,initial_size,-initial_size], # Left bottom back
-          [initial_size,initial_size,-initial_size], # Right bottom back
-          [initial_size,-initial_size,-initial_size]] # Right top back   
+#                        X             Y             Z
+points = np.array([[-initial_size,-initial_size, initial_size], # Left top back
+                   [-initial_size, initial_size, initial_size], # Left bottom back
+                   [ initial_size, initial_size, initial_size], # Right bottom back
+                   [ initial_size,-initial_size, initial_size], # Right top back
+                   [-initial_size,-initial_size,-initial_size], # Left top front
+                   [-initial_size, initial_size,-initial_size], # Left bottom front
+                   [ initial_size, initial_size,-initial_size], # Right bottom front
+                   [ initial_size,-initial_size,-initial_size]]) # Right top front
 
-def center_point(point, center_x=cube_center_x, center_y=cube_center_y):
-    return (point[0]+center_x,point[1]+center_y)
 
-while True:
-    clock.tick(60) # 60fps target framerate
-    screen.fill((0,0,0)) # Black background
+def normalize(vec):
+    # Assumes numpy array
+    total = 0
+    for i in range(len(vec)):
+        total += vec[i]
+    return vec/total
 
-    for e in pg.event.get():
-        if pg.mouse.get_pressed()[0]:
-                prev_m_x,prev_m_y = m_x,m_y
-                m_x,m_y = pg.mouse.get_pos()
-                
-                y += (m_x-prev_m_x)*movement_multiplier
-                x += (m_y-prev_m_y)*movement_multiplier
-        else:
-            m_x,m_y = pg.mouse.get_pos()
-
-    if drag: # Drag calculations
-        x *= (1-drag_rate)
-        y *= (1-drag_rate)
-        z *= (1-drag_rate)
-
+def render_wireframe():
     for i in range(4): # Drawing lines connecting the vertices (Assuming it's a cube)
-        pg.draw.line(screen, (255,255,255), (int(points[i][0])+cube_center_x, int(points[i][1])+cube_center_y) , (int(points[(i+1)%4][0])+cube_center_x, int(points[(i+1)%4][1])+cube_center_y) )
-        pg.draw.line(screen, (255,255,255), (int(points[i+4][0])+cube_center_x, int(points[i+4][1])+cube_center_y) , (int(points[((i+1)%4)+4][0])+cube_center_x, int(points[((i+1)%4)+4][1])+cube_center_y) )
-        pg.draw.line(screen, (255,255,255), (int(points[i][0])+cube_center_x, int(points[i][1])+cube_center_y) , (int(points[i+4][0])+cube_center_x, int(points[i+4][1])+cube_center_y) )
+        pg.draw.line(screen, (255,255,255), points[i][:2]+cube_center, points[(i+1)%4][:2]+cube_center)
+        pg.draw.line(screen, (255,255,255), points[i+4][:2]+cube_center, points[((i+1)%4)+4][:2]+cube_center)
+        pg.draw.line(screen, (255,255,255), points[i][:2]+cube_center, points[i+4][:2]+cube_center)
+
+def render_points():
+    for i in range(len(points)):
+        pg.draw.circle(screen, (np.cos([0.1+i,0.8+i,0.3+i])*0.25+0.75)*255, points[i][:2]+cube_center, 5)
+
+def render_face(v_i):
+    """ Renders face of cube given vertex indices that make-up that face. """
+
+    light_direction = np.array([1,1,-1])
 
     # Obtaining normal vector from the front face of the cube to determine direction. 
-    vec_a = np.array(points[0])-np.array(points[2])
-    vec_b = np.array(points[0])-np.array(points[1])
+    vec_a = points[v_i[0]]-points[v_i[1]]
+    vec_b = points[v_i[0]]-points[v_i[2]]
+    normal = np.cross(vec_a, vec_b)
 
-    normal = ( vec_a[1]*vec_b[2]-vec_a[2]*vec_b[1] , vec_a[2]*vec_b[0]-vec_a[0]*vec_b[2] , vec_a[0]*vec_b[1]-vec_a[1]*vec_b[0] )
+    if normal[2] <= 0: # If the z coordinate of the face normal points outside the screen, render the face
+        shading = ( np.dot(normal, light_direction)/(np.sqrt(normal.dot(normal))*np.sqrt(light_direction.dot(light_direction))) + 1 ) / 2
+        pg.draw.polygon(screen, [shading*200+50]*3, [points[v_i[0]][:2]+cube_center,points[v_i[1]][:2]+cube_center,points[v_i[2]][:2]+cube_center,points[v_i[3]][:2]+cube_center])
 
-    face = ( (points[1][0]+points[3][0])/2 , (points[1][1]+points[3][1])/2 ) # Center of cube face 
-    pg.draw.line(screen, (255,0,0),  center_point(face), center_point(np.array(normal)*0.0025), width=3)
-    
-    # Building the transformation matrix
-    transformation_matrix = np.array( ((1,0,0),(0,1,0),(0,0,1)) )
-    if x:
-        theta = np.radians(x)
-        c, s = np.cos(theta), np.sin(theta)
-        rotation_matrix_x = np.array( ((1,0,0),(0,c,-s),(0,s,c)) )
-        transformation_matrix = np.matmul(transformation_matrix,rotation_matrix_x)
-    if y:
-        theta = np.radians(y)
-        c, s = np.cos(-theta), np.sin(-theta)
-        rotation_matrix_y = np.array( ((c,0,s),(0,1,0),(-s,0,c)) )
-        transformation_matrix = np.matmul(transformation_matrix,rotation_matrix_y)
-    if z:
-        theta = np.radians(z)
-        c, s = np.cos(theta), np.sin(theta)
-        rotation_matrix_z = np.array( ((c,-s,0),(s,c,0),(0,0,1)) )
-        transformation_matrix = np.matmul(transformation_matrix,rotation_matrix_z)
-    if scale:
-        scale_matrix = np.array( ((scale,0,0),(0,scale,0),(0,0,scale)) )
-        transformation_matrix = np.matmul(transformation_matrix,scale_matrix)
-    if shear:
-        shear_matrix = np.array( ((1,sxy,sxz),(syx,1,syz),(szx,szy,1)) )
-        transformation_matrix = np.matmul(transformation_matrix,shear_matrix)
+def render_faces():
+    render_face([3,2,1,0]) # back
+    render_face([0,4,7,3]) # top
+    render_face([4,5,6,7]) # front
+    render_face([0,1,5,4]) # left-side
+    render_face([2,3,7,6]) # right-side
+    render_face([1,2,6,5]) # bottom
 
-    # Applying the transformation matrix to each vertex
-    for i in range(len(points)):
-        points[i] = np.matmul(points[i],transformation_matrix)
-        # This is to pervent a bug that causes pygame to crash if a value goes to 'infinity'. We catch it before that can happen.
-        if max(points[i]) >= 1e7 or min(points[i]) <= -1e7:
-            print("Value going off to +\- infinity")
-            exit()
 
-    pg.display.update()
-quit()
+if (__name__ == "__main__"):
+
+    pg.init()
+    screen_size = np.array((600,600))
+    screen = pg.display.set_mode(screen_size)
+    cube_center = np.array(screen_size//2)
+    clock = pg.time.Clock()
+    ticks = pg.time.get_ticks()
+
+    running = True
+    while running:
+        clock.tick(60) # 60fps target framerate
+        screen.fill((0,0,0)) # Black background
+
+        for e in pg.event.get():
+            if pg.mouse.get_pressed()[0]:
+                    prev_m_x,prev_m_y = m_x,m_y
+                    m_x,m_y = pg.mouse.get_pos()
+                    
+                    y += (m_x-prev_m_x)*movement_multiplier
+                    x += (m_y-prev_m_y)*movement_multiplier
+            elif e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
+                running = False
+            else:
+                m_x,m_y = pg.mouse.get_pos()
+
+        if drag: # Drag calculations
+            x *= (1-drag_rate)
+            y *= (1-drag_rate)
+            z *= (1-drag_rate)
+
+        #render_faces()
+        #render_wireframe()
+        render_points()
+
+        # Building the transformation matrix
+        transformation_matrix = np.array( ((1,0,0),(0,1,0),(0,0,1)) )
+        if x:
+            theta = np.radians(x)
+            c, s = np.cos(theta), np.sin(theta)
+            rotation_matrix_x = np.array( ((1,0,0),(0,c,-s),(0,s,c)) )
+            transformation_matrix = np.matmul(transformation_matrix,rotation_matrix_x)
+        if y:
+            theta = np.radians(y)
+            c, s = np.cos(-theta), np.sin(-theta)
+            rotation_matrix_y = np.array( ((c,0,s),(0,1,0),(-s,0,c)) )
+            transformation_matrix = np.matmul(transformation_matrix,rotation_matrix_y)
+        if z:
+            theta = np.radians(z)
+            c, s = np.cos(theta), np.sin(theta)
+            rotation_matrix_z = np.array( ((c,-s,0),(s,c,0),(0,0,1)) )
+            transformation_matrix = np.matmul(transformation_matrix,rotation_matrix_z)
+        if scale:
+            scale_matrix = np.array( ((scale,0,0),(0,scale,0),(0,0,scale)) )
+            transformation_matrix = np.matmul(transformation_matrix,scale_matrix)
+        if shear:
+            shear_matrix = np.array( ((1,sxy,sxz),(syx,1,syz),(szx,szy,1)) )
+            transformation_matrix = np.matmul(transformation_matrix,shear_matrix)
+
+        # Applying the transformation matrix to each vertex
+        for i in range(len(points)):
+            points[i] = np.matmul(transformation_matrix,points[i])
+            # This is to prevent a bug that causes pygame to crash if a value goes to 'infinity'. We catch it before that can happen.
+            if max(points[i]) >= 1e7 or min(points[i]) <= -1e7:
+                print("Value going off to +\- infinity")
+                exit()
+
+        pg.display.update()
+    quit()
--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # 3D Cube Renderer
 
-A simple renderer for a 3D wire-frame cube using rotation matrices.
+A simple renderer for a 3D cube using rotation matrices in numpy.
 
 ## Example
 
-![Gif of cube rotating](docs/output.gif)
+Cube rendered with faces.
 
-Movement with mouse (mouse isn't visible). Normal vector on front face is visible. This is with zero drag.
+![Gif of cube with faces rotating](docs/faces.gif)
 
-![Gif of cube rotating](docs/output1.gif)
+Cube rendered where each vertex is a dot.
+
+![Gif of cube made of dots rotating](docs/dots.gif)
+
+Wireframe version of the cube rendered.
+
+![Gif of wireframe cube rotating](docs/output.gif)
+
+Irregular movement. Normal vector on front face is visible. This is with zero drag.
+
+![Gif of wireframe cube rotating](docs/output1.gif)
+
