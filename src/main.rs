e3af2f7e3e6b2f29ba0851aed73a73cdb0cb61c1
NathanS-Git <nathan.smith.compsci@gmail.com>
2023-02-26 10:10:15 -0500

    n-dimensional rotation done

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,29 @@
-use nannou::prelude::{App, Frame, Draw, WHITE, BLACK, pt2, map_range};
+use nannou::winit::platform::unix::x11::ffi::Depth;
+use nannou::{App, Frame};
+use nannou::geom::pt2;
+use nannou::math::map_range;
+use nannou::color::{WHITE, BLACK};
 use ndarray::prelude::*;
+
+const DIM: usize = 4;
 
 fn main() {
     nannou::sketch(view).run();
 }
 
 fn generate_hypercube_vertices() -> Array2<f32> {
-    const DIM: usize = 3;
-
     let mut vertices: Array2<f32> = Array2::zeros((DIM, (1 << DIM)));
 
-    // Generate the vertices of the hypercube
     for i in 0..DIM { 
         for j in 0..(1 << DIM) {
             vertices[[i,j]] = if (j % (1 << (i+1))) >= (1 << i) { -0.5 } else { 0.5 };
         }
     }
 
-    // Generate the edges of the hypercube
-    /*
-    Figuring out a method to find the edges given a vertex index...
-
-    0: 0 0 0
-    1: 1 0 0
-    2: 0 1 0
-    3: 1 1 0
-    4: 0 0 1
-    5: 1 0 1
-    6: 0 1 1 
-    7: 1 1 1
-
-    0 -> (1, 2, 4)
-    1 -> (0, 3, 5)
-    2 -> (0, 3, 6)
-    3 -> (1, 2, 7)
-    4 -> (0, 5, 6)
-    5 -> (1, 4, 7)
-    6 -> (2, 4, 7)
-    7 -> (3, 5, 6)
-
-
-    1: 0 0 0 0
-    2: 1 0 0 0
-    3: 0 1 0 0
-    4: 1 1 0 0
-    5: 0 0 1 0
-    6: 1 0 1 0
-    7: 0 1 1 0
-    8: 1 1 1 0
-    9: 0 0 0 1
-    10: 1 0 0 1
-    11: 0 1 0 1
-    12: 1 1 0 1
-    13: 0 0 1 1
-    14: 1 0 1 1
-    15: 0 1 1 1
-    16: 1 1 1 1
-
-    1 -> (2, 3, 5, 9)
-    2 -> (1, 4, 6, 10)
-    3 -> (1, 4, 7, 11)
-    4 -> (2, 3, 8, 12)
-    5 -> (1, 6, 7, 13)
-    6 -> (2, 5, 8, 14)
-    7 -> (3, 5, 8, 15)
-    8 -> (4, 6, 7, 16)
-    9 -> (1, 10, 11, 13)
-    10 -> (2, 9, 12, 14)
-    11 -> (3, 9, 12, 15)
-    12 -> (4, 10, 11, 16)
-    13 -> (5, 9, 14, 15)
-    14 -> (6, 10, 13, 16)
-    15 -> (7, 11, 13, 16)
-    16 -> (8, 12, 14, 15)
-
-    1 -> (2, 3, 5, 9, 17, 33, 65, 129, 257, 513, 1025, 2049, 4097, 8193, 16385, 32769)
-    2 -> (1, 4, 6, 10, 18, 34, 66, 130, 258, 514, 1026, 2050, 4098, 8194, 16386, 32770)
-    3 -> (1, 4, 7, 11, 19, 35, 67, 131, 259, 515, 1027, 2051, 4099, 8195, 16387, 32771)
-    4 -> (2, 3, 8, 12, 20, 36, 68, 132, 260, 516, 1028, 2052, 4100, 8196, 16388, 32772)
-    5 -> (1, 6, 7, 13, 21, 37, 69, 133, 261, 517, 1029, 2053, 4101, 8197, 16389, 32773)
-    6 -> (2, 5, 8, 14, 22, 38, 70, 134, 262, 518, 1030, 2054, 4102, 8198, 16390, 32774)
-    7 -> (3, 5, 8, 15, 23, 39, 71, 135, 263, 519, 1031, 2055, 4103, 8199, 16391, 32775)
-    8 -> (4, 6, 7, 16, 24, 40, 72, 136, 264, 520, 1032, 2056, 4104, 8200, 16392, 32776)
-    9 -> (1, 10, 11, 13, 25, 41, 73, 137, 265, 521, 1033, 2057, 4105, 8201, 16393, 32777)
-    10 -> (2, 9, 12, 14, 26, 42, 74, 138, 266, 522, 1034, 2058, 4106, 8202, 16394, 32778)
-    
-    */    
-
-
-    for i in 0..(1 << DIM) {
-        println!("{}: {},{},{}", i, i ^ 1, i ^ (1 << 1), i ^ (1 << 2));
-    }
-
-    println!("{:?}", vertices);
     return vertices;
 }
 
-fn construct_rotation_matrix() -> Array2<f32> {
-    const DIM: usize = 3;
-
+fn construct_rotation_matrix(axis_1: usize, axis_2: usize, angle: f32) -> Array2<f32> {
     let mut rotation_matrix: Array2<f32> = Array2::zeros((DIM, DIM));
 
     for i in 0..DIM {
@@ -107,39 +32,59 @@ fn construct_rotation_matrix() -> Array2<f32> {
         }
     }
 
-    println!("{:?}", rotation_matrix);
+    // Make rotations occur in different directions
+    if axis_2 % 2 == 0 {
+        rotation_matrix[[axis_1,axis_2]] = f32::sin(angle);
+        rotation_matrix[[axis_2,axis_1]] = -f32::sin(angle);
+    } else {
+        rotation_matrix[[axis_1,axis_2]] = -f32::sin(angle);
+        rotation_matrix[[axis_2,axis_1]] = f32::sin(angle);
+    }
+
+    rotation_matrix[[axis_1,axis_1]] = f32::cos(angle);
+    rotation_matrix[[axis_2,axis_2]] = f32::cos(angle);
+
     return rotation_matrix; 
 }
 
 fn view(app: &App, frame: Frame) {
 
-
     let vertices: Array2<f32> = generate_hypercube_vertices();
-    let rotation_matrix_1: Array2<f32> = array![[1.0, 0.0, 0.0], [0.0, f32::cos(app.time), -f32::sin(app.time)], [0.0, f32::sin(app.time), f32::cos(app.time)]];
-    let rotation_matrix_2: Array2<f32> = array![[f32::cos(app.time), 0.0, f32::sin(app.time)], [0.0, 1.0, 0.0], [-f32::sin(app.time), 0.0, f32::cos(app.time)]];
-    let rotation_matrix_3: Array2<f32> = array![[f32::cos(app.time), -f32::sin(app.time), 0.0], [f32::sin(app.time), f32::cos(app.time), 0.0], [0.0, 0.0, 1.0]];
+    
+    let mut rotation_matrix = Array2::zeros((DIM, DIM));
 
-    let rotation_matrix = rotation_matrix_1.dot(&rotation_matrix_2).dot(&rotation_matrix_3);
+    for i in 0..DIM {
+        for j in 0..DIM {
+            rotation_matrix[[i,j]] = if i == j { 1.0 } else { 0.0 };
+        }
+    }
+    
+    let time = app.time*0.3;
+    for i in 0..DIM {
+        for j in (i+1)..DIM {
+            rotation_matrix = rotation_matrix.dot(&construct_rotation_matrix(i, j, time));
+        }
+    }
 
+    let vertices = rotation_matrix.dot(&vertices);
     let draw = app.draw();
 
-    // Build up the rotation matrix
-    
     draw.background().color(WHITE);
-
-    //let sine = app.time.sin();
 
     let vertices = rotation_matrix.dot(&vertices);
 
-    let boundary = app.window_rect();
+    let z_depth = 1.5;
 
-    const DIM: usize = 3;
     for i in 0..(1 << DIM) {
         for j in 0..DIM {
-            let x_1 = map_range(vertices[[0, i]], -1.0, 1.0, boundary.left(), boundary.right());
-            let x_2 = map_range(vertices[[0, i ^ (1 << j)]], -1.0, 1.0, boundary.left(), boundary.right());
-            let y_1 = map_range(vertices[[1, i]], -1.0, 1.0, boundary.bottom(), boundary.top());
-            let y_2 = map_range(vertices[[1, i ^ (1 << j)]], -1.0, 1.0, boundary.bottom(), boundary.top());
+            // Perspective project the vertex
+            // not Orthographically project the vertex
+
+            let x_1 = map_range((vertices[[0, i]]*z_depth) / (z_depth - vertices[[2, i]]), -1.0, 1.0, -100.0, 100.0);
+            let x_2 = map_range((vertices[[0, i ^ (1 << j)]]*z_depth) / (z_depth - vertices[[2, i ^(1 << j)]]), -1.0, 1.0, -100.0, 100.0);
+
+            let y_1 = map_range((vertices[[1, i]]*z_depth) / (z_depth - vertices[[2, i]]), -1.0, 1.0, -100.0, 100.0);
+            let y_2 = map_range((vertices[[1, i ^ (1 << j)]]*z_depth) / (z_depth - vertices[[2, i ^(1 << j)]]), -1.0, 1.0, -100.0, 100.0);
             draw.line()
                 .start(pt2(x_1, y_1))
                 .end(pt2(x_2, y_2))
