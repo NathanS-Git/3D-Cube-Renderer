c6259fd3e640088ebb1e91b4254b1abf4b242610
NathanS-Git <nathan.smith.compsci@gmail.com>
2023-04-02 13:04:19 -0400

    Move vertex generation outside view & better identity init

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ use nannou::geom::pt2;
 use nannou::math::map_range;
 use nannou::color::{WHITE, BLACK};
 use ndarray::prelude::*;
+use lazy_static::lazy_static;
 
-const DIM: usize = 10;
+const DIM: usize = 4;
+
+lazy_static! {
+    static ref VERTICES: Array2<f32> = generate_hypercube_vertices();
+}
 
 fn main() {
     nannou::sketch(view).run();
@@ -13,42 +18,34 @@ fn main() {
 fn generate_hypercube_vertices() -> Array2<f32> {
     let mut vertices: Array2<f32> = Array2::zeros((DIM, (1 << DIM)));
 
-    for i in 0..DIM { 
+    for i in 0..DIM {
         for j in 0..(1 << DIM) {
             vertices[[i,j]] = if (j % (1 << (i+1))) >= (1 << i) { -0.5 } else { 0.5 };
         }
     }
 
-    return vertices;
+    vertices
 }
 
 fn construct_rotation_matrix(axis_1: usize, axis_2: usize, angle: f32) -> Array2<f32> {
-    let mut rotation_matrix: Array2<f32> = Array2::zeros((DIM, DIM));
-
-    for i in 0..DIM {
-        rotation_matrix[[i,i]] = 1.0;
-    }
+    let mut rotation_matrix: Array2<f32> = Array2::eye(DIM);
 
     // Make the rotation flip every other time
     let angle = if axis_1 % 2 == 0 { angle } else { -angle };
-    
-    rotation_matrix[[axis_1,axis_2]] = f32::sin(angle);
-    rotation_matrix[[axis_2,axis_1]] = -f32::sin(angle);
-    rotation_matrix[[axis_1,axis_1]] = f32::cos(angle);
-    rotation_matrix[[axis_2,axis_2]] = f32::cos(angle);
 
-    return rotation_matrix; 
+    rotation_matrix[[axis_1, axis_2]] = f32::sin(angle);
+    rotation_matrix[[axis_2, axis_1]] = -f32::sin(angle);
+    rotation_matrix[[axis_1, axis_1]] = f32::cos(angle);
+    rotation_matrix[[axis_2, axis_2]] = f32::cos(angle);
+
+    rotation_matrix
 }
 
 fn view(app: &App, frame: Frame) {
-
-    let vertices: Array2<f32> = generate_hypercube_vertices();
+    let vertices = &*VERTICES;
 
     // Initialize the rotation matrix to the identity matrix
-    let mut rotation_matrix = Array2::zeros((DIM, DIM));
-    for i in 0..DIM {
-        rotation_matrix[[i,i]] = 1.0;
-    }
+    let mut rotation_matrix = Array2::eye(DIM);
     
     let time = app.time*0.5;
     for i in 0..DIM {
@@ -58,16 +55,15 @@ fn view(app: &App, frame: Frame) {
     }
 
     // Rotate the hypercube
-    let vertices = rotation_matrix.dot(&vertices);
+    let vertices = rotation_matrix.dot(vertices);
 
-    // Draw the hypercube 
     let draw = app.draw();
     draw.background().color(WHITE);
 
     // Perspective or Orthographic projection
     let perspective_project = true;
     if perspective_project {
-        assert!(DIM >= 3, "Perspective projection only works in +3 dimensions");
+        assert!(DIM >= 3, "Perspective projection only works in >2 dimensions");
     }
 
     let z_depth = if perspective_project { 1.5 } else { 1.0 };
@@ -78,12 +74,12 @@ fn view(app: &App, frame: Frame) {
             let denominator_1 = if perspective_project { z_depth - vertices[[2, i]] } else { 1.0 };
             let denominator_2 = if perspective_project { z_depth - vertices[[2, i ^ (1 << j)]] } else { 1.0 };
 
-            let x_1 = map_range((vertices[[0, i]]*z_depth) / denominator_1, -1.0, 1.0, -zoom, zoom);
-            let x_2 = map_range((vertices[[0, i ^ (1 << j)]]*z_depth) / denominator_2, -1.0, 1.0, -zoom, zoom);
+            let x_1 = map_range((vertices[[0, i]] * z_depth) / denominator_1, -1.0, 1.0, -zoom, zoom);
+            let x_2 = map_range((vertices[[0, i ^ (1 << j)]] * z_depth) / denominator_2, -1.0, 1.0, -zoom, zoom);
 
-            let y_1 = map_range((vertices[[1, i]]*z_depth) / denominator_1, -1.0, 1.0, -zoom, zoom);
-            let y_2 = map_range((vertices[[1, i ^ (1 << j)]]*z_depth) / denominator_2, -1.0, 1.0, -zoom, zoom);
-            
+            let y_1 = map_range((vertices[[1, i]] * z_depth) / denominator_1, -1.0, 1.0, -zoom, zoom);
+            let y_2 = map_range((vertices[[1, i ^ (1 << j)]] * z_depth) / denominator_2, -1.0, 1.0, -zoom, zoom);
+
             draw.line()
                 .start(pt2(x_1, y_1))
                 .end(pt2(x_2, y_2))
@@ -91,7 +87,5 @@ fn view(app: &App, frame: Frame) {
                 .color(BLACK);
         }
     }
-
     draw.to_frame(app, &frame).unwrap();
-    
 }
